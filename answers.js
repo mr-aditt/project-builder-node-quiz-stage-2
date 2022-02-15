8ff8cab0c984d6b503024523fd765ca1f6a40953
mr-aditt <mr.aditt@outlook.com>
2022-02-15 13:35:09 +0530

    Added: Update and delete

--- answers.js
+++
@@ -1,7 +0,0 @@
-const answers_Query = [
-  { id: 1, answer: "Rhopalocera" },
-  { id: 2, answer: "5,778 K" },
-  { id: 3, answer: "Vince Vaughn, Owen Wilson" },
-  { id: 4, answer: "Madrid" },
-  { id: 5, answer: "White, Burnt Orange" },
-];
--- index.js
+++ index.js
@@ -6,33 +6,134 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 
 const app = express();
-app.get("/", async function(req, res) {
-    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fl6wx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-    const client = new MongoClient(uri);
 
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fl6wx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+app.get("/", async function(req, res){
+    console.log("Logged-in:",client.s.options.credentials.username);
+    try {
+        await client.connect();
+        console.log("Connected Successfully");
+        res.json({
+            username: client.s.options.credentials.username
+        })
+    }catch (error){
+        console.log(error)
+    }finally {
+        await client.close();
+        console.log("Connection Closed Successfully!")
+    }
+})
+// To insert data
+app.get("/insert", async function(req, res) {
+    console.log("Logged-in:",client.s.options.credentials.username);
+    try {
+        await client.connect();
+        console.log("Connected Successfully");
+        var collection_status = {};
+        const db = client.db("quiz");
+        const questions = db.collection("questions");
+        const options = db.collection("options");
+        const answers = db.collection("answers");
+        for (var [key, value] of Object.entries(data)) {
+            const result = await eval(key).insertMany(value, { ordered: true });
+            if(result){collection_status[key] = true}
+            else{collection_status[key] = false}
+            console.log(`${result.insertedCount} documents were inserted in ${eval(key).namespace}`);
+        }
+        res.json(collection_status);
+    }catch (error){
+        console.log(error)
+    }finally {
+        await client.close();
+        console.log("Connection Closed Successfully!")
+    }
+});
+
+// To view data
+app.get("/search/:collection/:id", async function(req, res) {
+    console.log("Logged-in:",client.s.options.credentials.username);
+    const Id = parseInt(req.params.id)
     try {
         await client.connect();
         console.log("Connected Successfully");
 
         const db = client.db("quiz");
-        const questions = db.collection("questions");
-        const options = db.collection("options");
-        const answers = db.collection("answers");
-
-        for (var [key, value] of Object.entries(data)) {
-            const result = await eval(key).insertMany(value, { ordered: true });
-            console.log(`${result.insertedCount} documents were inserted in ${eval(key).namespace}`);
-            res.json({message: `${result.insertedCount} documents were inserted in ${eval(key).namespace}`,
-                data: value
+        const table = db.collection(req.params.collection);
+        const result = table.findOne({id:Id});
+        if(result){
+            res.json({
+                message: "Record FOUND",
+                result
+            });
+        }else{
+            res.json({
+                message: "FAILED to FIND"
             });
         }
-        
+    }catch (error){
+        console.log(error)
+    }finally {
+        await client.close();
+        console.log("Connection Closed Successfully!")
+    }
+});
 
+// To Update
+app.put("/update/:id", async function(req, res) {
+    console.log("Logged-in:",client.s.options.credentials.username);
+    const Id = parseInt(req.params.id)
+    try {
+        await client.connect();
+        console.log("Connected Successfully");
+
+        const db = client.db("quiz");
+        const record = db.collection("questions").findOneAndUpdate({id: Id},{$set:{question: req.body.question}});
+        if(record){
+            res.json({
+                message: "Record UPDATED",
+                result
+            });
+        }else{
+            res.json({
+                message: "FAILED to UPDATE"
+            });
+        }
     }catch (error){
         console.log(error)
     }finally {
         // Ensures that the client will close when you finish/error
         await client.close();
+        console.log("Connection Closed Successfully!")
+    }
+});
+
+app.delete("/delete/:collection/:id", async function(req, res) {
+    console.log("Logged-in:",client.s.options.credentials.username);
+    const Id = parseInt(req.params.id)
+    try {
+        await client.connect();
+        console.log("Connected Successfully");
+
+        const db = client.db("quiz");
+        const record = db.collection(req.params,collection).deleteOne({id: Id});
+        if(record){
+            res.json({
+                message: "Record DELETED",
+                result
+            });
+        }else{
+            res.json({
+                message: "FAILED to DELETE"
+            });
+        }
+    }catch (error){
+        console.log(error)
+    }finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+        console.log("Connection Closed Successfully!")
     }
 });
 
